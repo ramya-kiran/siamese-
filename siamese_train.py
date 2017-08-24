4165984f0af7f36cd2f2dd108713377db8a37da8
Ramya Rao <ramrao@umail.iu.edu>
2017-08-24 17:33:28 -0400

    added true and false positives calculation

--- siamese_train.py
+++ siamese_train.py
@@ -7,6 +7,7 @@ import time
 from siamese_model import *
 
 MARGIN = 0.5
+THRESHOLD = 1
 
 if __name__ == '__main__':
     # Read arguments
@@ -39,9 +40,22 @@ if __name__ == '__main__':
 
     loss = tf.multiply((1- y), distance) + tf.multiply(y, tf.square((tf.maximum(0.0, MARGIN - distance_root))))
 
-    loss_val = tf.reduce_sum(loss)
+    loss_val = tf.reduce_mean(loss)
     # Train
     train = tf.train.AdamOptimizer(1e-4).minimize(loss_val)
+
+    # calculate true positives
+    total_positives = args.batch_size - tf.reduce_sum(y)
+    predicted = distance >= THRESHOLD
+    actual = tf.cast(y, tf.bool)
+
+    true_positives = args.batch_size - tf.reduce_sum(tf.cast(tf.logical_or(predicted, actual), tf.float32))
+
+    true_positive_prop = true_positives/total_positives
+
+    # calculate false positives
+    false_positives = (args.batch_size - tf.reduce_sum(tf.cast(predicted, tf.float32))) - true_positives
+    false_positives_prop = false_positives/(tf.reduce_sum(y))
 
     #tf.summary.scalar('loss', loss)
     #tf.summary.scalar('accu', accuracy)
@@ -58,10 +72,16 @@ if __name__ == '__main__':
         coord = tf.train.Coordinator()
         threads = tf.train.start_queue_runners(sess=sess, coord=coord)
 
+        roc_file = open("roc_file.txt", "a")
+
         for i in range(args.epochs):
             images, labels = sess.run(batch)
             images1, images2, labels1 = split_images(images, args.batch_size, labels)
             sess.run(train, feed_dict={X1: images1, X2: images2, y: labels1})
+            roc_file.write(str(false_positives_prop))
+            roc_file.write(" ")
+            roc_file.write(str(true_positives_prop))
+            roc_file.write("\n")
             #s = sess.run(merged_summary, feed_dict={X1: images1, X2: images2, y: labels1})
             #writer.add_summary(s, i)
             
